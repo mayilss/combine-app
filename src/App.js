780ac9bd2488eee3149f5a515b785f8c5d559daf
mayilss <mayilsafarzada@gmail.com>
2023-01-13 17:34:26 +0400

    project setup and crud examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [hats, setHats] = useState([])
+  // const [hat, setHat] = useState({})
+
+  // const create = obj => {
+  //   axios.post(process.env.REACT_APP_DB_URL + '/hats.json', obj)
+  // }
+
+  // const read = () => {
+  //   const res = axios
+  //     .get(process.env.REACT_APP_DB_URL + '/hats.json')
+  //     .then(res => {
+  //       const loadedData = []
+
+  //       for (const dataKey in res.data) {
+  //         loadedData.push({ id: dataKey, name: res.data[dataKey].name })
+  //       }
+  //       setHats(loadedData)
+  //     })
+  // }
+
+  // const readById = id => {
+  //   axios
+  //     .get(process.env.REACT_APP_DB_URL + '/hats/' + id + '.json')
+  //     .then(res => {
+  //       setHat(res.data)
+  //     })
+  // }
+
+  // const deleteById = id => {
+  //   axios.delete(process.env.REACT_APP_DB_URL + '/hats/' + id + '.json')
+  // }
+
+  // const update = (id, obj) => {
+  //   axios.put(process.env.REACT_APP_DB_URL + '/hats/' + id + '.json', obj)
+  // }
+  return <div className="App"></div>
 }
 
-export default App;
+export default App
